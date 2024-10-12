a4c03766abe61edddcc243c1f95bae17a05fd3ba
Vivi2210 <vmoren2210@gmail.com>
2024-10-12 10:58:36 -0500

    Update CustomsExceptions.java

--- Main/src/main/java/com/mycompany/main/CustomsExceptions.java
+++ Main/src/main/java/com/mycompany/main/CustomsExceptions.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.main;
 
-/**
- *
- * @author ssuaz
- */
 public class CustomsExceptions {
 
     class MemberNotFoundException extends Exception {
